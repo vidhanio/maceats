46c300e2799a8c42dfb9f26e2eada342eec0fa0b
Woze Parrot <wozeparrot@gmail.com>
2022-10-31 23:25:45 -0400

    feat: nix build docker

--- flake.nix
+++ flake.nix
@@ -19,11 +19,12 @@
         };
 
         craneLib = crane.lib.${system};
-        src = ./backend;
+        src = ./backend/maceats-server;
 
         # Build dependencies
         cargoArtifacts = craneLib.buildDepsOnly {
           inherit src;
+          cargoLock = ./backend/Cargo.lock;
 
           nativeBuildInputs = with pkgs; [
             pkg-config
@@ -37,6 +38,7 @@
         # Build the crate
         backend = craneLib.buildPackage {
           inherit cargoArtifacts src;
+          cargoLock = ./backend/Cargo.lock;
 
           nativeBuildInputs = with pkgs; [
             pkg-config
@@ -47,12 +49,26 @@
           ];
         };
       in
-      {
+      rec {
         packages.backend = backend;
         packages.default = backend;
 
         apps.default = flake-utils.lib.mkApp {
           drv = backend;
+        };
+
+        dockerContainers.backend = pkgs.dockerTools.buildLayeredImage {
+          name = "app";
+          contents = [
+            pkgs.dockerTools.fakeNss
+            packages.backend
+          ];
+          config = {
+            Cmd = [ apps.default.program ];
+            ExposedPorts = {
+              "8080/tcp" = { };
+            };
+          };
         };
 
         devShells.default = pkgs.mkShell {
